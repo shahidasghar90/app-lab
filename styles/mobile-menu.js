544eaf010290149560bd0b293ba22585d01b5483
Shahid Asghar <shahidasghar90@yahoo.com>
2022-09-08 18:35:42 +0200

    mobile button

---
+++ styles/mobile-menu.js
@@ -0,0 +1,17 @@
+function myFunction () {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+// function myFunction () {
+//     var x = document.getElementById("myLinks");
+//     if (x.style.flex === "auto") {
+//         x.style.flex = "none";
+//     } else {
+//         x.style.flex = "auto";
+//     }
+// }
